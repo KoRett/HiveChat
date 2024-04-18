6d600a99d9bd714c51ae247f87920f3ccf8ca018
KoRett <kashencevrostik@gmail.com>
2024-04-18 22:54:34 +0300

    Add dokka:)

--- app/src/main/java/com/razratanchiki/hivechat/MainActivity.kt
+++ app/src/main/java/com/razratanchiki/hivechat/MainActivity.kt
@@ -3,6 +3,9 @@ package com.razratanchiki.hivechat
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+/**
+ * @author kashentsev.r.s
+ */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
